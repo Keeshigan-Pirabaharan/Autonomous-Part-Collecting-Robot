44d2ef89d2e45dccb40724216f0af1971877e99b
Keeshigan Pirabaharan <spkeeshi@gmail.com>
2022-02-19 12:43:06 -0500

    Add files via upload
    IR_Decoder Test

---
+++ IR_DECODER.ino
@@ -0,0 +1,22 @@
+#include <IRremote.h>
+
+const int RECV_PIN = A3;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+int H = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+  irrecv.blink13(true);
+}
+
+void loop()
+{
+  if(irrecv.decode(&results))
+  {
+    Serial.println(results.value, HEX); //INT?
+    irrecv.resume();
+  }
+}
