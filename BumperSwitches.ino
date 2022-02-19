5c4ceb8df3759add7e3b991a3a973937ae847233
Keeshigan Pirabaharan <spkeeshi@gmail.com>
2022-02-19 12:38:36 -0500

    Add files via upload
    Test Bumper Switches

---
+++ BumperSwitches.ino
@@ -0,0 +1,15 @@
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(3, INPUT);
+  digitalWrite(3, HIGH);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  if(digitalRead(3))
+  Serial.println("High");
+  else
+  Serial.println("Low");
+}
